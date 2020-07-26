40f43f93d8cfc594b2a363492570928b64ff916b
tom_zhou <tom_zhou@qq.com>
2020-07-26 15:30:59 +0800

    Test：提交至本地库

---
+++ HelloGit/src/main/java/com/zhou/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.zhou.git;
+
+/**
+ * @author Tommey周
+ * @create 2020-07-26 15:10
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
